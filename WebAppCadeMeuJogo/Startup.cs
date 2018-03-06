a8db205a3f4e8eb10aca207b06086cf57a33990b
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2018-03-06 17:13:33 -0300

    add Projeto WebAppCadeMeuJogo

---
+++ WebAppCadeMeuJogo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppCadeMeuJogo.Startup))]
+namespace WebAppCadeMeuJogo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
