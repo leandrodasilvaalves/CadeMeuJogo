370dd07f54843b82a200535c6136eb61cac37201
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2018-03-10 23:03:56 -0300

    Ajustes Account

--- WebAppCadeMeuJogo/Models/IdentityModels.cs
+++ WebAppCadeMeuJogo/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace WebAppCadeMeuJogo.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("cademeujogoConexao", throwIfV1Schema: false)
         {
         }
 
